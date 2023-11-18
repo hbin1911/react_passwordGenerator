30b2bb5272a096e0a2f48ea4db3961a6a48d9c76
hbin1911 <rajas.mulik@gmail.com>
2023-11-18 15:44:27 +0530

    created ui

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+
+  const [length, setLength] = useState(4)
+  const [checkboxData, setCheckboxData] = useState([
+    {title: "Include Uppercase Letters", state:false},
+    {title: "Include Lowercase Letters", state:false},
+    {title: "Include Numbers", state:false},
+    {title: "Include Symbols ", state:false},
+  ])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="header">
+        <div className="title">KhrOyZ</div>
+        <button className="copyBtn" onClick={()=>{}}>copy</button>
+      </div>
+      <div className="charLength">
+        <span>
+          <label>Character Length</label>
+          <label>{length}</label>
+        </span>
+          <input type="range" 
+          min='4'
+           max='20'
+           value={length}
+           onChange={(e)=>setLength(e.target.value)}
+          />
+      </div>
+      <div className="checkboxes">
+        {checkboxData.map((checkbox, i)=>{
+          return <div key={i}>
+            <input type="checkbox" checked={checkbox.state}/>
+            <label>{checkbox.title}</label>
+          </div>
+        })}
+      </div>
+      <button className="generateBtn" onClick={()=>{}}>Generate Password</button>
     </div>
   );
 }
